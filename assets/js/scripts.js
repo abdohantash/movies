1f2a51541687117282793f777f9f4c7eefbcd239
abdohantash <abdohantash9999@gmail.com>
2024-06-01 16:38:10 +0300

    edit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,7 +5,8 @@ var swiper_logos = new Swiper(".swiper-logos", {
   watchSlidesProgress: true,
 });
 var swiper_poster = new Swiper(".swiper-poster", {
-  spaceBetween: 10,
+  speed: 600,
+  parallax: true,
   thumbs: {
     swiper: swiper_logos,
   },
