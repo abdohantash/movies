85ad0591a2a88d6d4035f85750e20749708b8c9e
abdohantash <abdohantash9999@gmail.com>
2024-06-03 22:17:10 +0300

    edit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,22 @@
+$(window).on('scroll', function() {
+  if ($(window).scrollTop() > 0) {
+      $('.header-box').addClass('scroll-header');
+  } else {
+      $('.header-box').removeClass('scroll-header');
+  }
+});
+
+$(document).ready(function() {
+  $('.btn-search').on('click', function() {
+    $('.search-box').addClass('active');
+  });
+
+  $('.close-search').on('click', function() {
+    $('.search-box').removeClass('active');
+  });
+});
+
+
 var swiper_logos = new Swiper(".swiper-logos", {
   spaceBetween: 20,
   slidesPerView: 5,
@@ -7,7 +26,31 @@ var swiper_logos = new Swiper(".swiper-logos", {
 var swiper_poster = new Swiper(".swiper-poster", {
   speed: 600,
   parallax: true,
+  loop:true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
   thumbs: {
     swiper: swiper_logos,
+  },
+});
+
+
+var swiper_movies = new Swiper(".swiper-movies", {
+  slidesPerView: "auto",
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var top_ten = new Swiper(".top_ten .swiper", {
+  slidesPerView: "auto",
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
